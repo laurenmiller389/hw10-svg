15d6f9b4c94c8c09ba394779b593945da5baadb0
Lauren Miller <lmiller90@fordham.edu>
2023-06-14 16:01:20 -0400

    completed indexing of all lib files

--- index.js
+++ index.js
@@ -1,5 +1,52 @@
-const CLI = require('./lib/cli.js');
+//CLI is going to link everything together based from command line (prompts and tasks)
+const inquirer = require('inquirer');
+const fs = require('fs');
 
-const cli = new CLI();
+const questions = [
+    {
+        name: "text",
+        type: "input",
+        message: "enter up to five characters for text: "
+    },
+    {
+        name: "shape",
+        type: "input",
+        message: "choose from the below shapes",
+        choices: ['Circle', 'Triangle', 'Square']
+    },
+    {
+        name: "textColor",
+        type: "input",
+        message: "enter a text color (type color keyword or hex code)"
+    },    
+    {
+        name: "shapeColor",
+        type: "input",
+        message: "enter a shape color (type color keyword or hex code)"
+    },
+];
 
-cli.run();
+//fs write file
+function writeToFile(fileName, data) {
+    fs.writeFile(fileName, data, (err) => {
+        if (err) throw err;
+        console.log()
+    })
+}
+
+//handle answers
+const handleAnswers = (answers) => {
+    if (answers.text.length > 5) {
+        console.log("Text must be 5 characters at most");
+        init();
+    } else {
+        //writeToFile('logo.svg', )
+    }
+}
+
+function init() {
+    inquirer
+    .prompt(questions).then(handleAnswers);
+}
+
+init();
